50a4e3b1acba5d59d15cab3e8039131f2770d210
Aad Abdurahman <kats.misch@gmail.com>
2025-07-24 16:47:39 +0700

    Recovery

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    OK
                     {{ __('You are logged in!') }}
                 </div>
             </div>
